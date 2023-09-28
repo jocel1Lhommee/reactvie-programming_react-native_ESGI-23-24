6bea029e0cb0b1c254f5df73f5dc2afc69edf965
MORVAN Loïs <lmorvan6@myges.fr>
2023-09-28 14:40:30 +0200

    Ajout du nom dans formualire

--- myfirstapp/src/components/formulaire.js
+++ myfirstapp/src/components/formulaire.js
@@ -1,10 +1,18 @@
 function bienvenue() {
-    alert('Bienvenue à ESGI Party !');
+    alert('❤️Bienvenue à ESGI Party !❤️');
+}
+
+function monnomsaffiche(nameGuest) {
+    nameGuest.preventDefault();
+    alert(`❤️Bienvenue à ESGI Party ${nameGuest.target['nom'].value} !❤️`);
 }
 
 function Container() {
     return (<div>
-        <form className='formulaire'>
+        <form className='formulaire' onSubmit={monnomsaffiche}>
+            <div className='nameGuest'>
+                <input type='text' name='nom' placeholder='Votre nom' />
+            </div>
             <div className='signIn'>
                 <label>
                     S'enregistrer :
