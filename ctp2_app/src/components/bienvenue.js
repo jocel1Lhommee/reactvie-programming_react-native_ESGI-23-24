a03d99850240278794c23ac6eb432efffc6ef867
Jocel1 lhommee <jocelyn.lhommee@insa-rennes.fr>
2023-09-28 13:01:43 +0200

    Manipulation du DOM

---
+++ ctp2_app/src/components/bienvenue.js
@@ -0,0 +1,4 @@
+function Bienvenue(props){
+    return <h1>Bonjour, {props.name}</h1>;
+}
+export default Bienvenue;
--- myfirstapp/src/components/App.js
+++ myfirstapp/src/components/App.js
@@ -1,4 +1,4 @@
-import Header from './header';
+import Header from './Header';
 import Footer from './footer';
 import React from 'react';
 import './style.css';
@@ -6,17 +6,6 @@ import Formulaire from './formulaire';
 
 function App() {
   return (<React.Fragment><Header /><Formulaire /><Footer /></React.Fragment>)
-}
-
-export default App;
-
-import './App.css';
-import Header from './Header';
-
-function App() {
-  return (
-    <Header></Header>
-  );
 }
 
 export default App;
