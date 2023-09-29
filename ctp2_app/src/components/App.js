86cc7f474dc9c70a2351850c91db8b0fd21110b3
Jocel1 lhommee <jocelyn.lhommee@insa-rennes.fr>
2023-09-29 11:03:35 +0200

    props+ boutton state

---
+++ ctp2_app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Bienvenue from './bienvenue';
+
+function App() {
+  return (
+    <React.Fragment><Bienvenue name="Maha"
+    text="Enseignant permanant"
+    date="5/5/2022" /></React.Fragment>
+  )
+}
+
+export default App;
