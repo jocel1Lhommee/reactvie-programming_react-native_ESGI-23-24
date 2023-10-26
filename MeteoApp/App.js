921acfac3e55ac0a7adb5b4bad44ccb8aed72f67
MORVAN Loïs <lmorvan6@myges.fr>
2023-10-26 09:57:56 +0200

    Remove MeteoApp

--- MeteoApp/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Weather from './components/weather'
-export default class App extends React.Component {
-  render() {
-    return (
-      <Weather />
-    );
-  }
-}
