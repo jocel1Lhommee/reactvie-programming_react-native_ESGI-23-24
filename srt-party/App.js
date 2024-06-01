17c8f784053a36b4c3c76b7eaed68ef6afddab43
Jocel1 <jocel1.lhommee@gmail.com>
2024-06-01 16:24:19 +0200

    clean

--- srt-party/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Accueil from './components/accueil'
-export default class App extends React.Component {
-  render() {
-    return (
-      < Accueil />
-    );
-  }
-}
